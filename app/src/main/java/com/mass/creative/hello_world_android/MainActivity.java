9a3a1331072e5450d19fad2aee022ce68f90b429
Ryan Gerber <ryangerber1990@wmail.com>
2019-01-05 12:11:56 +0200

    Added toast to button

--- app/src/main/java/com/mass/creative/hello_world_android/MainActivity.java
+++ app/src/main/java/com/mass/creative/hello_world_android/MainActivity.java
@@ -2,12 +2,26 @@ package com.mass.creative.hello_world_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button helloBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloBtn = (Button) findViewById(R.id.helloBtn);
+
+        helloBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"How do you do?",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
